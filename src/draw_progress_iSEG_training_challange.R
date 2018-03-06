1bace8a525a12d8f06ff3d3ecfb65c4e3f2e4df0
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2018-03-06 18:52:23 -0500

    Added copyright notice

--- src/draw_progress_iSEG_training_challange.R
+++ src/draw_progress_iSEG_training_challange.R
@@ -1,3 +1,24 @@
+################################ MNI Header ##################################-
+## @NAME       :  draw_progress_iSEG_training_challange.R
+## @DESCRIPTION:  Draw training progress curves
+## @COPYRIGHT  :
+##               Copyright 2017 Vladimir Fonov, McConnell Brain Imaging Centre, 
+##               Montreal Neurological Institute, McGill University.
+##
+##     This program is free software: you can redistribute it and/or modify
+##     it under the terms of the GNU General Public License as published by
+##     the Free Software Foundation, either version 3 of the License, or
+##     (at your option) any later version.
+## 
+##     This program is distributed in the hope that it will be useful,
+##     but WITHOUT ANY WARRANTY; without even the implied warranty of
+##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##     GNU General Public License for more details.
+## 
+##     You should have received a copy of the GNU General Public License
+##     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##             
+########################################################################
 library(tidyverse)
 library(zoo)
 library(grid)
@@ -113,5 +134,3 @@ ggplot(data=cvvk,aes(y=Kappa,x=Structure_S))+
     theme_bw()
 
 #write.csv(cvvk,file=paste(out_prefix,'cvv_ind.csv',sep='_'),quote=F)
-
-
