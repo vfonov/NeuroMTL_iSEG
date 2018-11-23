3f6fe3a10d75ed8aa65edc1d47a2e390673ac654
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2018-11-23 11:19:57 -0500

    Added Copyright notice

--- src/apply_fcn.lua
+++ src/apply_fcn.lua
@@ -1,6 +1,26 @@
-require 'torch'
---require 'cutorch'
+-------------------------------- MNI Header -----------------------------------
+-- @NAME       :  apply_fcn.lua
+-- @DESCRIPTION:  Apply fully-convolutional network on a 3D volume
+-- @COPYRIGHT  :
+--               Copyright 2017 Vladimir Fonov, McConnell Brain Imaging Centre, 
+--               Montreal Neurological Institute, McGill University.
+--
+--     This program is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+-- 
+--     This program is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+-- 
+--     You should have received a copy of the GNU General Public License
+--     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--             
+------------------------------------------------------------------------
 
+require 'torch'
 require 'minc2_simple'
 paths.dofile('tiles.lua')
 
